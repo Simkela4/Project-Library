42c2f0df8fe2127b781c72255555508aa189cfd5
Simkela4 <simicdusan9@gmail.com>
2025-06-27 00:15:32 +0200

    Loop and display

--- lib.js
+++ lib.js
@@ -22,3 +22,29 @@ function addBookToLibrary(title, author,pages,read) {
     myLibrary.push(book);
     return book;
   }
+
+  function loop(){
+    const container=document.getElementById("book-list");
+    container.innerHTML="";
+
+    myLibrary.forEach((book)=>{
+        const card=document.createElement("div");
+        card.classList.add("book-card");
+        
+        card.innerHTML = `
+        <h3>${book.title}</h3>
+        <p><strong>Author:</strong> ${book.author}</p>
+        <p><strong>Pages:</strong> ${book.pages}</p>
+        <p><strong>Status:</strong> ${book.read ? "Read" : "Not read yet"}</p>
+      `;
+      container.appendChild(card);
+    });
+
+  }
+addBookToLibrary("1984", "George Orwell", 328, true);
+addBookToLibrary("Na Drini ćuprija", "Ivo Andrić", 412, true);
+addBookToLibrary("The Great Gatsby", "F. Scott Fitzgerald", 180, true);
+addBookToLibrary("To Kill a Mockingbird", "Harper Lee", 281, false);
+addBookToLibrary("Brave New World", "Aldous Huxley", 311, true);
+
+loop();
